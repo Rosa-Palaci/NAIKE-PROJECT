68399d44a2f38d9183f0c263b704c70d732d2bb4
Rosa Palaci <palacibelrose431@gmail.com>
2023-03-03 12:45:43 -0600

    Last version

---
+++ carusel.js
@@ -0,0 +1,44 @@
+// DOM
+const swiper = document.querySelector("#swiper");
+const like = document.querySelector("#like");
+const dislike = document.querySelector("#dislike");
+
+// constants
+const urls = [
+  "../assets/1.svg",
+  "../assets/2.svg",
+  "../assets/3.svg",
+  "../assets/4.svg",
+  "../assets/5.svg",
+];
+
+// variables
+let cardCount = 0;
+
+// functions
+function appendNewCard() {
+  const card = new Card({
+    imageUrl: urls[cardCount % 5],
+    onDismiss: appendNewCard,
+    onLike: () => {
+      like.style.animationPlayState = "running";
+      like.classList.toggle("trigger");
+    },
+    onDislike: () => {
+      dislike.style.animationPlayState = "running";
+      dislike.classList.toggle("trigger");
+    },
+  });
+  swiper.append(card.element);
+  cardCount++;
+
+  const cards = swiper.querySelectorAll(".card:not(.dismissing)");
+  cards.forEach((card, index) => {
+    card.style.setProperty("--i", index);
+  });
+}
+
+// first 5 cards
+for (let i = 0; i < 5; i++) {
+  appendNewCard();
+}
